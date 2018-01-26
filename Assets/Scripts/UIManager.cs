ca05551629863625a675a9c7e4f9981f5987a69d
CamDHall <camerondylan.h@gmail.com>
2018-01-26 11:27:26 -0600

    Registeration Form

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Color backgroundColor;
+    public GameObject registerForm, quizForm, scrollView;
+
+	public void RegisterScene()
+    {
+        SceneManager.LoadScene("Registeration");
+    }
+
+    public void LogInScene()
+    {
+        SceneManager.LoadScene("login");
+    }
+
+    public void Register()
+    {
+        scrollView.GetComponentInChildren<Image>().color = backgroundColor;
+        registerForm.SetActive(false);
+        quizForm.SetActive(true);
+    }
 }
