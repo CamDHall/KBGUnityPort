3c6883f18553a41363e9ac86dbf1f80d34f14826
CamDHall <camerondylan.h@gmail.com>
2018-01-31 14:00:31 -0600

    Color Scroll

--- Assets/Scripts/AvatarController.cs
+++ Assets/Scripts/AvatarController.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
+public enum Parts { Skin, Hair, Cloth, Hat};
 public class AvatarController : MonoBehaviour {
 
     GameObject avatar;
+    Color hairColor, mainColor, secondaryColor;
+	
+    Parts activePart = Parts.Skin;
+    public ScrollRect vw;
+    public RectTransform skin, hair, hat, cloth;
 
-	// Use this for initialization
 	void Start () {
         GameObject temp;
+
         // For testing just this scene
         if (UserManager.Instance == null)
         {
@@ -31,9 +39,31 @@ public class AvatarController : MonoBehaviour {
         avatar = Instantiate(temp, transform);
         avatar.transform.localPosition = Vector2.zero;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void ChangeScrollContent()
+    {
+        GameObject clicked = EventSystem.current.currentSelectedGameObject;
+        string cName = clicked.name;
+
+        RectTransform temp = vw.content;
+        temp.gameObject.SetActive(false);
+
+        if (cName == "SkinTone")
+        {
+            vw.content = skin;
+            skin.gameObject.SetActive(true);
+        } else if(cName == "Hair")
+        {
+            vw.content = hair;
+            hair.gameObject.SetActive(true);
+        } else if(cName == "Hat")
+        {
+            vw.content = hat;
+            hat.gameObject.SetActive(true);
+        } else if(cName == "Shirt")
+        {
+            vw.content = cloth;
+            cloth.gameObject.SetActive(true);
+        }
+    }
 }
